3e482f39498f60ac335d9858a04562fb5c81b4ce
Federico Mestrone <federico@moodsdesign.com>
2022-12-13 14:04:50 +0000

    Removed framework for custom QR Codes
    Framework is now in separate project

--- src/main/kotlin/org/nekosoft/utils/qrcode/QrCodeDrawStyle.kt
+++
@@ -1,9 +0,0 @@
-package org.nekosoft.utils.qrcode
-
-import io.github.g0dkar.qrcode.internals.QRCodeSquare
-import io.github.g0dkar.qrcode.render.QRCodeGraphics
-
-interface QrCodeDrawStyle {
-    fun validate(options: QrCodeOptions): Boolean
-    fun render(cellData: QRCodeSquare, canvas: QRCodeGraphics, rawData: Array<Array<QRCodeSquare?>>, options: QrCodeOptions)
-}
